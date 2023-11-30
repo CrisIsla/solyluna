662010084ee933391a33f1cd66dfa8f2659794a7
Nicolás I. Peña Henríquez <cs.cubes.2001@gmail.com>
2023-11-30 01:06:51 -0300

    create config file

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+// Here we'll define de configuration for the app and the environment variables.
+
+// const port = process.env.PORT || 3000;
+// const host = process.env.HOST || 'localhost';
+// ...
