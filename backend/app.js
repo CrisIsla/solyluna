3c0b0dd181a57684800c077c31b500ab270e1fa1
Nicolás I. Peña Henríquez <cs.cubes.2001@gmail.com>
2023-11-30 01:29:07 -0300

    delete comment

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-// const port = process.env.PORT || 3000;
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
