a9a02179019ad450a7acd4388cdfe2ed89e11020
CrisIsla <cristobal.isla@ug.uchile.cl>
2023-12-08 17:05:50 -0300

    add db connection

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
+require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
-
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
-});
 
 // Use the products router for requests
 const products = require('./routes/products');
 app.use('/products', products);
+
+// Connect to database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('Server is listening on port', process.env.PORT);
+        });
+    })
+    .catch((error) => {
+        console.log(error)
+    })
