d3f09469382cd2477967ae8127acb0ee1e5850d4
CrisIsla <cristobal.isla@ug.uchile.cl>
2023-12-09 17:15:56 -0300

    create get, post and delete methods

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
+app.use(express.json())
 
 // Use the products router for requests
 const products = require('./routes/products');
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../models/Product')
 
 // Get all products
-router.get('/', (req, res) => {
-    res.send('GET method on products route');
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find()
+        res.status(200).json(products)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 });
 
 // Get a specific product
@@ -12,9 +18,30 @@ router.get('/:id', (req, res) => {
 });
 
 // Create a new product
-router.post('/', (req, res) => {
-    res.send('POST method on products route');
+router.post('/', async (req, res) => {
+    const product = new Product({
+        barCode: req.body.barCode,
+        name: req.body.name,
+        price: req.body.price
+    })
+
+    try {
+        const newProduct = await product.save()
+        res.status(201).json(newProduct)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
 });
+
+// Delete a specific product
+router.delete('/:barCode', async (req, res) => {
+    try {
+        await Product.deleteOne(req.params)
+        res.status(200).json({ message: `deleted product with barCode ${req.params.barCode}` })
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
 
 // Update a product (we need to decide if we want to use PUT or PATCH)
 router.put('/:id', (req, res) => {
