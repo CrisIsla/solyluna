10f97d05884d0e4193e5bbf87525a24dac5267a7
CrisIsla <cristobal.isla@ug.uchile.cl>
2023-12-10 17:32:36 -0300

    delete ;

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,19 @@
 require('dotenv').config()
-const express = require('express');
+const express = require('express')
 const mongoose = require('mongoose')
-const app = express();
+const app = express()
 app.use(express.json())
 
 // Use the products router for requests
-const products = require('./routes/products');
-app.use('/products', products);
+const products = require('./routes/products')
+app.use('/products', products)
 
 // Connect to database
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(process.env.PORT, () => {
             console.log('Server is listening on port', process.env.PORT);
-        });
+        })
     })
     .catch((error) => {
         console.log(error)
